f659748cff88778fb13ca43bbf82d634949e83c7
shohei22 <toyosima11031103@hotmail.co.jp>
2023-06-06 14:52:25 +0900

    first commit

---
+++ home.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Greenleaf Garden</title>
+<!-- Bootstrap CSS -->
+<link rel="stylesheet"
+	href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<!-- Custom CSS -->
+<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<!-- Navigation Menu -->
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<a class="navbar-brand" href="#"> <img
+			src="https://free-icons.net/wp-content/uploads/2020/09/nature028.png"
+			alt="Logo" width="30" height="30" class="d-inline-block align-top">
+			Greenleaf Garden
+		</a>
+
+		<button class="navbar-toggler" type="button" data-toggle="collapse"
+			data-target="#navbarNav" aria-controls="navbarNav"
+			aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item active"><a class="nav-link" href="#">Home</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Menu</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Garden Gallery</a>
+				</li>
+				<li class="nav-item"><a class="nav-link" href="#">Reservations</a></li>
+				<li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
+			</ul>
+		</div>
+	</nav>
+
+	<!-- Jumbotron -->
+	<div class="jumbotron jumbotron-fluid text-center">
+		<div class="container">
+			<h2 class="display-4">Discover the Essence of Nature</h2>
+			<p class="lead">Indulge in the flavors of our garden-fresh dishes.</p>
+			<p class="catchphrase">Experience the Bliss of Nature's Bounty</p>
+			<p class="promotion">Limited Time Offer: Special Dinner Set 20% OFF</p>
+			<a class="btn btn-primary btn-lg" href="#" role="button">View Menu</a>
+		</div>
+	</div>
+
+	<!-- Main Content -->
+	<div id="main-content" class="main-content container">
+		<h1 class="mt-5">Welcome to Greenleaf Garden!</h1>
+		<p class="lead">Experience luxury moments with beautiful gardens and
+			fresh ingredients.</p>
+
+		<div class="row mt-5">
+			<div class="col-md-6">
+				<h2>About Us</h2>
+				<p>Greenleaf Garden is a one-of-a-kind restaurant where nature meets
+					culinary art. Our mission is to provide a unique dining experience
+					that celebrates the beauty of nature and showcases the flavors of
+					fresh, locally sourced ingredients.</p>
+				<p>Step into our enchanting garden oasis and be transported to a
+					world of serenity and indulgence. Immerse yourself in the lush
+					greenery, soothing sounds of nature, and exquisite aromas that will
+					awaken your senses.</p>
+			</div>
+			<div class="col-md-6">
+				<h2>Our Services</h2>
+				<ul>
+					<li>Seasonal Farm-to-Table Menu: We believe in serving dishes made
+						with the freshest ingredients sourced directly from local farms.
+						Our menu changes with the seasons, ensuring that you experience
+						the best flavors each time you visit.</li>
+					<li>Elegant Dining Spaces: Whether you prefer a romantic dinner
+						under the stars or a private gathering with friends, we offer a
+						variety of elegant dining spaces to suit your needs. Our garden
+						terrace, private cabanas, and indoor dining area provide the
+						perfect ambiance for any occasion.</li>
+					<li>Cocktail Bar: Unwind and enjoy handcrafted cocktails at our
+						stylish cocktail bar. Our talented mixologists are dedicated to
+						creating unique and refreshing drinks that perfectly complement
+						your dining experience.</li>
+					<li>Events and Celebrations: Make your special moments
+						unforgettable at Greenleaf Garden. From intimate celebrations to
+						corporate events, our dedicated team will help you plan and
+						execute a memorable experience tailored to your preferences.</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<!-- Signature Menu -->
+	<section id="signature-menu" class="mt-5">
+		<div id="signature-menu-content" class="signature-menu-content container py-4">
+			<h2 class="text-center mb-4">Signature Menu</h2>
+			<div class="row">
+				<div class="col-md-4">
+					<div class="card card-main-content">
+						<img src="https://hips.hearstapps.com/amapn-prod-jp.s3.amazonaws.com/elljp/elle-mig/images/gourmet/cooking/recipe/3617/269553-1-jpn-JP/_1.jpg?crop=1xw:0.5625xh;center,top&resize=1200:*"
+							class="card-img-top card-img-height" alt="Menu 1">
+						<div class="card-body">
+							<h5 class="card-title">グリーンガーデンサラダ</h5>
+							<p class="card-text">
+								新鮮な季節の野菜とハーブを使用したカラフルなサラダ。特製のグリーンリーフドレッシングと相性抜群です。</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card card-main-content">
+						<img src="https://cdn.sbfoods.co.jp/recipes/02934_l.jpg" class="card-img-top card-img-height" alt="Menu 2">
+						<div class="card-body">
+							<h5 class="card-title">ローズマリーチキン</h5>
+							<p class="card-text">オーブンでじっくり焼き上げた香ばしいローズマリーチキン。添えられた彩り野菜とのバランスが絶妙です。</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="card card-main-content">
+						<img src="https://www.niwacul.jp/fcontents/abc/original/blog_block_image/11/cabbage_grill_main02_5440.jpg" class="card-img-top card-img-height" alt="Menu 3">
+						<div class="card-body">
+							<h5 class="card-title">ガーデンハーブグリル</h5>
+							<p class="card-text">季節の野菜と共に、新鮮なハーブで香り高くグリルしたグリル料理。野菜の旨みを引き立てます。</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Footer -->
+	<footer class="footer mt-auto py-3 text-center bg-light">
+		<div class="container">
+			<p class="m-0">&copy; 2023 Greenleaf Garden. All rights reserved.</p>
+		</div>
+	</footer>
+
+	<!-- Bootstrap JS -->
+	<script
+		src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</body>
+</html>
